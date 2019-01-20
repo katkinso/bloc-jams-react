ee4f6c431755e8af2a361d3ef25f56f7a2b52379
katkinso <katyratkinson@yahoo.com>
2019-01-20 10:20:09 -0800

    checkpoint-3-Routing: complete

---
+++ src/components/Landing.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landing = () => (
+  <section className="landing">Landing page will go here</section>
+);
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Library = () => (
+  <section className="library">Library page will go here</section>
+);
+
+export default Library;
